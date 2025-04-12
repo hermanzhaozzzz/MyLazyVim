995fac25c24c18ef7bd9d4470591abe7a64beb78
Huanan Herman Zhao <hermanzhaozzzz@gmail.com>
2025-04-12 16:51:03 +0800

    add keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,7 +13,27 @@ map("i", "<C-e>", "<C-o>A", { desc = "Go to end of the line", noremap = true, si
 
 -- FIXME 在 MacOS 中和任务调度快捷键冲突
 -- Resize window using <ctrl> arrow keys
--- map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
--- map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
--- map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
--- map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+map("n", "<leader>z<Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<leader>z<Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<leader>z<Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<leader>z<Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Neovide mappings
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.g.neovide_hide_mouse_when_typing = true
+  -- Neovide mappings
+  local function adjust_neovide_scale(adjustment)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + adjustment
+  end
+
+  map("n", "<leader>zi", function()
+    adjust_neovide_scale(0.1)
+  end, { silent = true, nowait = true, desc = "For Neovide: Zoom in" })
+  map("n", "<leader>zo", function()
+    adjust_neovide_scale(-0.1)
+  end, { silent = true, nowait = true, desc = "For Neovide: Zoom out" })
+  map("n", "<leader>zr", function()
+    vim.g.neovide_scale_factor = 1
+  end, { silent = true, nowait = true, desc = "For Neovide: Reset zoom" })
+end
