84c85d80e8d989679661283bc9d786102bf70aff
wubeizhongxinghua <393811901@qq.com>
2025-04-25 16:15:41 +0800

    fix iron

--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -2,51 +2,47 @@ return {
   "hkupty/iron.nvim",
   config = function()
     local iron = require("iron.core")
+    local view = require("iron.view")
+    local common = require("iron.fts.common")
 
     iron.setup({
       config = {
-        -- Whether a repl should be discarded or not
         scratch_repl = true,
-        -- Your repl definitions come here
         repl_definition = {
           sh = {
-            -- Can be a table or a function that
-            -- returns a table (see below)
             command = { "zsh" },
           },
+          python = {
+            command = { "ipython", "--no-autoindent" },
+            format = common.bracketed_paste_python,
+            block_dividers = { "# %%", "#%%" },
+          },
         },
-        -- How the repl window will be displayed
         -- See below for more information
-        repl_open_cmd = require("iron.view").bottom(40),
+        repl_open_cmd = require("iron.view").right("%40"),
       },
-      -- Iron doesn't set keymaps by default anymore.
-      -- You can set them here or manually add keymaps to the functions in iron.core
       keymaps = {
-        send_motion = "<space>sc",
-        visual_send = "<space>sc",
-        send_file = "<space>sf",
-        send_line = "<space>sl",
-        send_mark = "<space>sm",
+        toggle_repl = "<space>rr", -- toggles the repl open and closed.
+        restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
+        send_motion = "<space>rc",
+        visual_send = "<space>rc",
+        send_code_block = "<space>rb",
+        send_code_block_and_move = "<space>rn",
+        send_file = "<space>rf",
+        send_line = "<space>rl",
+        send_mark = "<space>rm",
         mark_motion = "<space>mc",
         mark_visual = "<space>mc",
         remove_mark = "<space>md",
-        cr = "<space>s<cr>",
-        interrupt = "<space>s<space>",
-        exit = "<space>sq",
-        clear = "<space>cl",
+        cr = "<space>r<cr>",
+        interrupt = "<space>ri",
+        exit = "<space>rq",
+        clear = "<space>rL",
       },
-      -- If the highlight is on, you can change how it looks
-      -- For the available options, check nvim_set_hl
       highlight = {
         italic = true,
       },
       ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
     })
-
-    -- iron also has a list of commands, see :h iron-commands for all available commands
-    vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
-    vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
-    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
-    vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
   end,
 }
